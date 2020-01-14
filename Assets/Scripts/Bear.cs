79114b223d74cb3ab00b377957bf3af8db7a36e4
Kelly Ortiz Figueiredo <kellyortiz.figueiredo@gmail.com>
2020-01-14 20:58:54 -0300

    Imagens e código do jogo base

---
+++ Assets/Scripts/Bear.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bear : MonoBehaviour {
+
+    [SerializeField]
+
+    private Transform BearPlace;
+    private Vector2 initialPosition;
+    private Vector2 mousePosition;
+    private float deltaX, deltaY;
+    public static bool locked;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPosition = transform.position;     
+    }
+
+    private void OnMouseDown()
+    {
+        if (!locked)
+        {
+            deltaX = Camera.main.ScreenToWorldPoint(Input.mousePosition).x - transform.position.x;
+            deltaY = Camera.main.ScreenToWorldPoint(Input.mousePosition).y - transform.position.y;
+        }
+    }
+
+    private void OnMouseDrag()
+    {
+        if (!locked)
+        {
+            mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = new Vector2(mousePosition.x - deltaX, mousePosition.y - deltaY);
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        if(Mathf.Abs(transform.position.x - BearPlace.position.x) <= 0.5f &&
+           Mathf.Abs(transform.position.y - BearPlace.position.y) <= 0.5f)
+        {
+            transform.position = new Vector2(BearPlace.position.x, BearPlace.position.y);
+            locked = true;
+        }
+        else
+        {
+            transform.position = new Vector2(initialPosition.x, initialPosition.y);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
